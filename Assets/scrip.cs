ff0a44d0701ddc3887e2cdad0f74d5801634fb8d
Matias Martini <m.martini.1294@gmail.com>
2021-01-29 22:27:12 -0300

    First Commit

---
+++ Assets/scrip.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrip : MonoBehaviour
+{
+
+    Renderer f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
